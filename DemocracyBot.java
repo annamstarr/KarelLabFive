dced87faa50671e39237257c4171f8f192f6eca4
jhstarr <jeffhstarr@gmail.com>
2017-09-21 15:06:58 -0700

    KarelLabFiveHome

--- DemocracyBot.java
+++ DemocracyBot.java
@@ -7,13 +7,78 @@ import kareltherobot.*;
  */
 public class DemocracyBot extends Robot
 {
+    private int beepers;
     public DemocracyBot(int st, int av, Direction dir, int numBeepers) {
         super(st, av, dir, numBeepers);
     }
     
     public void fixBallots() {
-        
+        for (int i=0; i<10; i++) {
+            move();
+            checkRoom();
+            reposition();
+        }
     }
+    public void checkRoom() {
+        if (nextToABeeper()) {
+            fillRooms();
+        }
+        else {
+            emptyRooms();
+        }
+    }
+    public void faceNorth() {
+        while (!facingNorth()) {
+            turnLeft();
+        }
+    }
+    public void faceSouth() {
+        while (!facingSouth()) {
+            turnLeft();
+        }
+    }
+    public void checkBeeperYes() {
+        if (!nextToABeeper()) {
+            putBeeper();
+        }
+    }
+    public void fillRooms() {
+        faceNorth();
+        move();
+        checkBeeperYes();
+        faceSouth();
+        while (!frontIsClear()) {
+            move();
+        }
+        checkBeeperYes();
+    }
+    public void emptyRooms() {
+        faceNorth();
+        move();
+        checkBeeperNo();
+        faceSouth();
+        while (!frontIsClear()) {
+            move();
+        }
+        checkBeeperNo();
+    }
+    public void checkBeeperNo() {
+        if (nextToABeeper()) {
+            pickBeeper();
+        }
+    }
+    public void reposition() {
+        faceNorth();
+        move();
+        faceEast();
+        move();
+    }
+    public void faceEast() {
+        while (!facingEast()) {
+            turnLeft();
+        }
+    }
+    
    
 }
 
